e41658209a7ee085d81d80c2fd2ce5d0d7e6da98
Dave <dave.f.hollander@gmail.com>
2019-08-13 15:34:19 -0400

    Update CPU unittest

--- tests/ForwardTest.jl
+++ tests/ForwardTest.jl
@@ -5,13 +5,29 @@ EmbDims = 128
 Vocab = Integer(1e4)
 Layers = 8
 Stacks = 1
-Model = SequenceModel(EmbDims, Vocab, TimeSteps, Layers, Stacks)
 
 X = map(a->Int32.(a), [[3,7,3,5], [8,6,4,5,2,1], [10,7,5], [11,3]]);
 Y = map(a->Int32.(a), [[3,7,3,5], [8,6,4,5,2,1], [10,7,5], [11,3]]);
-@time (output, context, decoutput, crosscontext) = Model(X, Y);
-println(size(output))
 
-# @time Model(X, Y);
+Input = SequenceFeed(EmbDims, Vocab, TimeSteps)
+Encode = Encoding(EmbDims, TimeSteps, Layers, Stacks)
+Decode = Decoding(EmbDims, TimeSteps, Layers, Stacks)
+Output = DenseProjection(Vocab)
+Attn = Attention(EmbDims)
 
-println(crosscontext)
+function Model(x::AbstractArray{T, 1}, y::AbstractArray{T, 1}) where T
+    EncSeqLens = SequenceLengths(x)
+    x = Input(x)
+    x, EncMask = Encode(x, EncSeqLens)
+    context = Attn(x .* EncMask)
+
+    DecSeqLens = SequenceLengths(y)
+    y = Input(y)
+    y = Decode(x, y, DecSeqLens, context, EncMask, Attn)
+
+    y = Output(y, Flux.gpu(Input.Emb.E))
+    return y
+end
+
+@time output = Model(X, Y);
+# println(output)
