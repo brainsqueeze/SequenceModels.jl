fde3d33bcaf239fecd9063324d6b12ac1c738a27
brainsqueeze <dave.f.hollander@gmail.com>
2019-08-19 22:47:13 -0400

    Temporary fix for culiteral_pow bug

--- src/TensorOps.jl
+++ src/TensorOps.jl
@@ -7,14 +7,20 @@ end
 
 mean(x::AbstractArray, dims::Integer) = sum(x, dims=dims) / size(x, dims)
 
-function BatchLayerNorm(x::AbstractArray{T, 3} where T; ϵ = 1e-8, scale = 1.0, bias = 0)
+function BatchLayerNorm(x::AbstractArray{T, 3} where T; ϵ::Float32 = 1e-8, scale::Float32 = 1.0, bias::Float32 = 0)
     ϵ = Float32(ϵ)
     scale = Float32(scale)
     bias = Float32(bias)
 
     average = mean(x, 3)
-    variance = mean((x .- average) .^ 2, 3)
-    xnorm = (x .- average) ./ Float32.((variance .+ ϵ) .^ Float32(0.5))
+
+    # todo: get this working properly with exponents after culiteral_pow is fixed for CuArrays.jl
+    # https://github.com/JuliaGPU/CuArrays.jl/pull/380
+    # variance = mean((x .- average) .^ 2, 3)
+    variance = mean((x .- average) .* (x .- average), 3)
+
+    # xnorm = (x .- average) ./ Float32.((variance .+ ϵ) .^ Float32(0.5))
+    xnorm = (x .- average) ./ Float32.(sqrt.(variance .+ ϵ))
     return xnorm .* scale .+ bias
 end
 
@@ -22,7 +28,8 @@ function Projection(x::AbstractArray{T, 3}, P::AbstractArray{T, 2}) where T
     TimeSteps = size(x, 1)
 
     Inner = BatchAffineNoBias(x, P)
-    NormSqr = sum(P .^ 2, dims=1)
+    # NormSqr = sum(P .^ 2, dims=1)
+    NormSqr = sum(P .* P, dims=1)
     NormSqr = repeat(NormSqr, outer=[TimeSteps, 1])
     α = Inner ./ NormSqr
 
